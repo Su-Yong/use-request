3d66d4cc97e408ebda66d8fb8bb6e68cdcd9e0c2
Su-Yong <simssy2205@gmail.com>
2022-04-17 03:48:40 +0000

    add deep equal

---
+++ src/utils.ts
@@ -0,0 +1,18 @@
+export const isEqual = (a: any, b: any): boolean => {
+  if (a && b && typeof a === 'object' && typeof b === 'object') {
+    if (Array.isArray(a) && Array.isArray(b)) {
+      return a.length === b.length && a.every((_, index) => isEqual(a[index], b[index]));
+    } else {
+      const aKeys = Object.keys(a);
+      const bKeys = Object.keys(b);
+
+      if (aKeys.length === bKeys.length && aKeys.every((aKey) => bKeys.some((bKey) => aKey === bKey))) {
+        return aKeys.every((key) => isEqual(a[key], b[key]));
+      }
+
+      return false;
+    }
+  }
+
+  return a === b;
+};
